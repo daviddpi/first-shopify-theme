5216ccaba33f0a090cbd8caec8896f3da921e366
daviddpi <piscopodavid97@gmail.com>
2024-01-22 16:39:39 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./layout/*.liquid",
+        "./sections/*.liquid",
+        "./snippets/*.liquid",
+        "./templates/*.liquid",
+        "./templates/customers/*.liquid",
+
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
